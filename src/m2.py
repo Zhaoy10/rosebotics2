988893ad7afc1e52227a451fdc60d38632d51bce
fangr <fangr@rose-hulman.edu>
2018-10-24 16:16:53 -0400

    frnb

--- src/m2.py
+++ src/m2.py
@@ -9,6 +9,19 @@ import time
 
 def main():
     """ Runs YOUR specific part of the project """
+    run_test_trace()
 
+def run_test_trace():
+    frnb = rb.Snatch3rRobot()
+    left = 60
+    right = 60
+    while frnb.color_sensor.get_color() == 1:
+        frnb.drive_system.start_moving(left, right)
+        break
+
+    while frnb.color_sensor.get_color() != 1:
+        left = left+1
+        frnb.drive_system.start_moving(left, right)
+        break
 
 main()
