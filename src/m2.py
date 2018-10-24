07d6510d6c7816dbd58c00dea9ed62d3b4d4d315
fangr <fangr@rose-hulman.edu>
2018-10-24 16:28:02 -0400

    frnb

--- src/m2.py
+++ src/m2.py
@@ -13,17 +13,21 @@ def main():
 
 def run_test_trace():
     frnb = rb.Snatch3rRobot()
-    left = 60
-    right = 60
+
     while frnb.touch_sensor.get_value() == 0:
+        left = 60
+        right = 50
         while frnb.color_sensor.get_color() == 1:
             frnb.drive_system.start_moving(left, right)
             break
 
         while frnb.color_sensor.get_color() != 1:
-            left = left+5
+            if left < 90:
+                left = left+5
             frnb.drive_system.start_moving(left, right)
             break
+        break
+
     frnb.drive_system.stop_moving()
 
 main()
