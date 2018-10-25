ffaf58f2bad0d5992976cc4eb5f5403e7cd4d872
xias <xias@rose-hulman.edu>
2018-10-25 15:59:57 -0400

    touch sensor

--- src/rosebotics.py
+++ src/rosebotics.py
@@ -181,11 +181,15 @@ class TouchSensor(low_level_rb.TouchSensor):
 
     def wait_until_pressed(self):
         """ Waits (doing nothing new) until the touch sensor is pressed. """
-        # TODO.
+        while True:
+            if self.get_value() == 1:
+                break
 
     def wait_until_released(self):
         """ Waits (doing nothing new) until the touch sensor is released. """
-        # TODO
+        while True:
+            if self.get_value() == 0:
+                break
 
 
 class Camera(object):
