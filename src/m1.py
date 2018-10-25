cdd894af3620966b1cd640d25a8cd8c8bc51c186
zhaoy10 <Zhaoy10@rose-hulman.edu>
2018-10-25 16:49:02 -0400

    inches

--- src/m1.py
+++ src/m1.py
@@ -10,8 +10,8 @@ import time
 def main():
     """ Runs YOUR specific part of the project """
     frjb=rb.Snatch3rRobot()
-    frjb.drive_system.start_moving(100,100)
-    time.sleep(2)
+    frjb.drive_system.start_moving(100,-100)
+    time.sleep(5)
     frjb.drive_system.stop_moving()
 
 main()
