32d73fc7af416cb2c5f0a868c048d507f3daebdf
fangr <fangr@rose-hulman.edu>
2018-10-25 15:59:26 -0400

    frnb

--- src/m3.py
+++ src/m3.py
@@ -9,6 +9,23 @@ import time
 
 def main():
     """ Runs YOUR specific part of the project """
+    run_test_trace()
 
+def run_test_trace():
+    frnb = rb.Snatch3rRobot()
+
+    for k in range(1000):
+        left = 24
+        right = 20
+        frnb.drive_system.start_moving(left, right)
+
+        while frnb.color_sensor.get_color() != 1:
+            if left < 60:
+                left = left+1
+            frnb.drive_system.start_moving(left, right)
+            print(frnb.color_sensor.get_color())
+            break
+
+        frnb.drive_system.stop_moving()
 
 main()
