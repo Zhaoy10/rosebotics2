8426d0588f9d38e9b48a03f943b52e18743932e8
fangr <fangr@rose-hulman.edu>
2018-11-05 16:41:14 -0500

    New stuff

---
+++ src/Sprint 2 mini-applications.py
@@ -0,0 +1,24 @@
+import time
+import rosebotics_even_newer as rb
+
+
+def main():
+    print('Testing beep when wave hands')
+    run_test_beep_when_wave_hands()
+    time.sleep(3)
+    print('Testing beep when see big objects')
+    run_test_beep_when_sees_big()
+    time.sleep(3)
+
+
+def run_test_beep_when_wave_hands():
+    robot = rb.Snatch3rRobot
+    robot.camera
+
+
+
+def run_test_beep_when_sees_big():
+
+
+
+main()
